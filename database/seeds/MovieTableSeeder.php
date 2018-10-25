cc7671cb400dba6884ae95a4f0e4272876e73ba2
Hepnerd <nbhepner@geneva.edu>
2018-10-24 23:07:40 -0400

    Added seeds and created views for movies

--- database/seeds/MovieTableSeeder.php
+++ database/seeds/MovieTableSeeder.php
@@ -14,11 +14,21 @@ class MovieTableSeeder extends Seeder
         DB::table('movies')->delete();
 
         Movie::create(array(
-            'title' => 'Lone Survivor',
+            'title' => 'Avatar',
             'rating' => '4 out of 5',
             'length' => '121',
             'onDVD' => '1',
-            'onBlueRay' => '0'
+            'onBlueRay' => '0',
+            'coverPhoto' => '/images/avatar.jpg',
+        ));
+
+        Movie::create(array(
+            'title' => 'Great Gatsby',
+            'rating' => '4.5 out of 5',
+            'length' => '123',
+            'onDVD' => '1',
+            'onBlueRay' => '0',
+            'coverPhoto' => '/images/gatsby.jpg',
         ));
 
     }
--- resources/views/movieIndex.blade.php
+++ resources/views/movieIndex.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+
     @foreach($movie as $movies)
-        <h1>{{ $movies['title'] }}</h1>
+  <div class="row">
+        <div class="col-sm">
+    <img class="movieCover" style="padding-bottom: 20px;" src={{ $movies['coverPhoto'] }}></div>
+        <div class="col-sm">
+        <h1>Title: {{ $movies['title'] }}</h1>
+        <h1>Rating: {{ $movies['rating'] }}</h1>
+        <h1>Length: {{ $movies['length'] }} Minutes</h1>
+        <h1>Description: {{ $movies['description'] }}</h1>
+        <h1>On DVD: {{ $movies['onDVD'] }}</h1>
+        <h1>On Blue Ray: {{ $movies['onBlueRay'] }}</h1>
+      </div>
+    </div>
     @endforeach
+  </div>
 @endsection
