63e5fdeae0bad4b731a228c9e92c9adb07edef60
brettwebb63 <brett.webb@geneva.edu>
2018-10-30 21:32:52 -0400

    Create Read Update and Delete Sorta work

--- resources/views/movieIndex.blade.php
+++ resources/views/movieIndex.blade.php
@@ -1,42 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+    @extends('layouts.app')
+
+@section('content')
     <div class="container">
-        <h1 style="text-align:center; text-decoration: underline">Movie List</h1>
 
-        <table class="Movie_table" align="center">
-            <thead>
-            <tr>
-                <th style="text-align:center;">Movie's Title</th>
-                <th style="text-align:center;">Description</th>
-                <th style="text-align:center;">Button Bar</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($Movie as $Movies)
-
-                <tr>
-                    <td>{{ $Movies['title'] }}</td>
-                    <td style="text-align:center;">{{ $Movies['description'] }}</td>
-                    <td><a href="{{route('movie.edit', $Movies['id'])}}" id="MovieEditButton" name="MovieEditButton" class="btn btn-primary">Edit</a></td>
-                    <td>
-                        <form method="POST" action="{{route('movie.destroy', $Movies['id'])}}">
-                            @csrf
-                            <fieldset>
-                                <!-- Button -->
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label" for="deleteButton"></label>
-                                    <div class="col-md-4">
-                                        <button id="submitDeleteButton" name="submitDeleteButton" class="btn btn-danger">Delete</button>
-                                    </div>
-                                </div>
-                            </fieldset>
-                        </form>
-                        </td>
-                </tr>
-
-            @endforeach
-            </tbody>
-        </table>
+        @foreach($movie as $movies)
+            <div class="row">
+                <div class="col-sm">
+                    <img class="movieCover" style="padding-bottom: 20px;" src={{ $movies['coverPhoto'] }}></div>
+                <div class="col-sm">
+                    <h1>Title: {{ $movies['title'] }}</h1>
+                    <h1>Rating: {{ $movies['rating'] }}</h1>
+                    <h1>Length: {{ $movies['length'] }} Minutes</h1>
+                    <h1>Description: {{ $movies['description'] }}</h1>
+                    <h1>On DVD: {{ $movies['onDVD'] }}</h1>
+                    <h1>On Blue Ray: {{ $movies['onBlueRay'] }}</h1>
+                </div>
+            </div>
+        @endforeach
     </div>
+@endsection
 @endsection
