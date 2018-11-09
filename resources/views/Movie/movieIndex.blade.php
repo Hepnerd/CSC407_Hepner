3238e2575faeeb5dd72ae9816cd393518f2fda5d
Hepnerd <nbhepner@geneva.edu>
2018-11-08 20:13:09 -0500

    Getting closer to finishing the movie index page

--- resources/views/Movie/movieIndex.blade.php
+++ resources/views/Movie/movieIndex.blade.php
@@ -6,8 +6,19 @@
         @foreach($movie as $movies)
         <div class="flex-item">
 
-                    <img class="movieCover" style="padding-bottom: 20px;" src="/images/movie_{{$movies['id']}}.jpg">
+                    <img class="movieCover" src="/images/movie_{{$movies['id']}}.jpg">
+                    <table class="movieTable" cellpadding="0" cellspacing="0" border="0">
+                      <tr>
+                        <th>{{$movies['title']}}</th>
+                      </tr>
+                      <tr>
+                        <td>{{$movies['length']}}</td>
+                      </tr>
+                      <tr>
+                        <td>{{$movies['description']}}</td>
 
+                      </tr>
+                    </table>
 
 
 </div>
